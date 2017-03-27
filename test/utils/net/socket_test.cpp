6d54f2b8dd093564c68f3cb478ffe63e3052e110
horbie <horbie_huo@163.com>
2017-03-27 21:31:11 +0800

    build test

--- test/utils/net/socket_test.cpp
+++ test/utils/net/socket_test.cpp
@@ -5,3 +5,9 @@ TEST(FooTest, HandleNoneZeroInput)
     EXPECT_EQ(2, 2);
     EXPECT_EQ(6, 3+3);
 }
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
