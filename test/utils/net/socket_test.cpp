f8964255979feafcc77d3956fbc6c1e0f486c84c
horbie <horbie_huo@163.com>
2017-03-30 21:09:14 +0800

    unit test

--- test/utils/net/socket_test.cpp
+++ test/utils/net/socket_test.cpp
@@ -1,9 +1,34 @@
 #include "gtest/gtest.h"
+#include "socket.h"
+#include "ioloop.h"
 
 TEST(FooTest, HandleNoneZeroInput) {
   EXPECT_EQ(2, 2);
   EXPECT_EQ(6, 3 + 3);
 }
+
+#define SOCKET_CONTINER_CAPACITY 10
+
+class SocketUnittest: public testing::Test {
+  protected:
+  virtual void SetUp() {
+    m_io = NULL;
+    memset(m_sockets, 0, sizeof(m_sockets));
+  }
+  virtual void TearDown() {
+    m_io->Release();
+    for (int i = 0; i < SOCKET_CONTINER_CAPACITY; ++i) {
+      if (m_sockets[i]) {
+        delete m_sockets[i];
+        m_sockets[i] = NULL;
+      }
+      delete m_sockets;
+    }
+  }
+
+  IOLoop* m_io;
+  Socket* m_sockets[SOCKET_CONTINER_CAPACITY];
+};
 
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
