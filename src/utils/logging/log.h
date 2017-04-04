b1109eb868dc4d3a5a4de357bf5224128eda5935
horbie <horbie_huo@163.com>
2017-04-04 20:11:44 +0800

    add color to log

--- src/utils/logging/log.h
+++ src/utils/logging/log.h
@@ -6,12 +6,17 @@
 #include <string.h>
 #include <time.h>
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 namespace share_me_utils {
 
 #ifdef _WIN32
 #define localtime localtime_s
 #define sprintf sprintf_s
 #endif // _WIN32
+#define COLOR unsigned short
 
 #define LOG_BUFFER_LENGTH 1024
 
@@ -20,6 +25,7 @@ public:
   enum LEVEL {
     S_TRACE = 0,
     S_DEBUG,
+    S_WARN,
     S_INFO,
     S_ERROR,
     S_FATAL,
@@ -39,7 +45,10 @@ private:
   void formatString(const char *format, ...);
   int generatePrefix(const char *filename, const char *funcname,
                      const int lineno, int level);
-
+  void setColor(int level);
+  void resetColor();
+  bool initColor();
+ 
   char m_logBuffer[2 * LOG_BUFFER_LENGTH];
   char m_prefixBuffer[LOG_BUFFER_LENGTH];
 
@@ -53,23 +62,28 @@ private:
   };
   char m_prefixSymbols[eTop];
   int m_prefixSwitchs[eTop];
+  COLOR m_levelColor[S_INVALID];
+  COLOR m_oldColorAttr;
   char *m_levelString[S_INVALID];
 };
 
 #define LOG_TRACE(formart, ...)                                                \
-  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_TRACE,       \
+  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_TRACE,     \
                                formart, __VA_ARGS__))
 #define LOG_DEBUG(formart, ...)                                                \
-  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_DEBUG,       \
+  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_DEBUG,     \
                                formart, __VA_ARGS__))
 #define LOG_INFO(formart, ...)                                                 \
-  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_INFO,        \
+  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_INFO,      \
+                               formart, __VA_ARGS__))
+#define LOG_WARN(formart, ...)                                                 \
+  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_DEBUG,     \
                                formart, __VA_ARGS__))
 #define LOG_ERROR(formart, ...)                                                \
-  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_ERROR,       \
+  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_ERROR,     \
                                formart, __VA_ARGS__))
 #define LOG_FATAL(formart, ...)                                                \
-  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_FATAL,       \
+  (Log::Instance()->LogContent(__FILE__, __LINE__, __func__, Log::S_FATAL,     \
                                formart, __VA_ARGS__))
 
 } // namespace share_me_utils
