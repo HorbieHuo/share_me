4c87f145947ea7bf43835824852ce9deab32920b
horbie <horbie_huo@163.com>
2017-04-06 22:21:27 +0800

    json

--- src/utils/json/pasertool.cpp
+++ src/utils/json/pasertool.cpp
@@ -3,14 +3,63 @@
 namespace share_me_utils {
 namespace json_inner {
 
-StateMachine::StateMachine() {}
+StateMachine::StateMachine() { init(); }
 StateMachine::~StateMachine() {}
-Value *StateMachine::Next(const char &c) { return nullptr; }
+bool StateMachine::init() {
+  m_deep = 0;
+  m_charMap.Clear();
+  m_charMap.Set('{');
+  m_charMap.Set('}');
+  m_charMap.Set('[');
+  m_charMap.Set(']');
+  m_charMap.Set(',');
+  m_charMap.Set(':');
+  m_charMap.Set('"');
+  return true;
+}
+int StateMachine::Next(const char &c) {
+  if (!m_charMap[c])
+    return 0;
+switch (c) {
+    case '{': {
+        break;
+    }
+    case '}': {
+        break;
+    }
+    case '[': {
+        break;
+    }
+    case ']': {
+        break;
+    }
+    case ',': {
+        break;
+    }
+    case ':': {
+        break;
+    }
+    case '"': {
+        break;
+    }
+    default: {
+        return 0;
+    }
+}
+  return 0;
+}
+
+int StateMachine::onIntoObject() { return 0; }
+int StateMachine::onOutObject() { return 0; }
+int StateMachine::onIntoArray() { return 0; }
+int StateMachine::onOutArray() { return 0; }
+int StateMachine::onNextElement() { return 0; }
 
 // ----------------------
 CharMap::CharMap() {}
 CharMap::~CharMap() {}
+void CharMap::Clear() { memset(m_map, 0, sizeof(m_map)); }
 
-bool CharMap::operator[](char c) { return true; }
+bool CharMap::operator[](const char &c) { return true; }
 }
 }
--- src/utils/json/pasertool.h
+++ src/utils/json/pasertool.h
@@ -3,14 +3,27 @@
 
 namespace share_me_utils {
 namespace json_inner {
+#define STATE_DIVIDE_ACTION
 class StateMachine {
 public:
   enum STATE {
     OBJECT = 1 << 0,
     ARRAY = 1 << 1,
+    KEY_ELEM = 1 << 2,
+    VALUE_ELEM = 1 << 3,
+    STR_ELEM = 1 << 4,
+    NUM_ELEM = 1 << 5,
+    IN_ELEM = 1 << 6,
+    OUT_ELEM = 1 << 7,
+    TOP_STATE = 1 << 8,
+  };
 
-    IN_ELEM = 1 << 2,
-    OUT_ELEM = 1 << 3,
+  enum ACTION {
+    INTO_OBJECT = TOP_STATE << 0,
+    OUT_OBJECT = TOP_STATE << 1,
+    INTO_ARRAY = TOP_STATE << 2,
+    OUT_ARRAY = TOP_STATE << 3,
+    NEXT_ELEM = TOP_STATE << 4,
   };
 
 public:
@@ -19,15 +32,26 @@ public:
   Value *Next(const char &c);
 
 private:
+  bool init();
+  int onIntoObject();
+  int onOutObject();
+  int onIntoArray();
+  int onOutArray();
+  int onNextElement();
+
+private:
   size_t m_deep;
+  CharMap m_charMap;
 };
 
 class CharMap {
 public:
   CharMap();
   ~CharMap();
+  void Clear();
+  bool Set(const char &c);
 
-  bool operator[](char c);
+  bool operator[](const char &c);
 
 private:
   char m_map[32];
