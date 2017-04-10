7e3887df0325fa7753edc292b4f26a81fc07557d
horbie <horbie_huo@163.com>
2017-04-10 21:44:37 +0800

    json build pass

--- src/utils/json/json.cpp
+++ src/utils/json/json.cpp
@@ -51,8 +51,8 @@ bool Json::Paser() {
     return false;
   int currentAction = 0;
   while (*textPos != '\0') {
-    bool isSpecialChar = specialCharMap[*textPos];
-    if (isSpecialChar) {
+    // bool isSpecialChar = specialCharMap[*textPos];
+    if (specialCharMap[*textPos]) {
       currentAction = m_stateMachine.Next(*textPos);
       //TODO dispatch action
     }
